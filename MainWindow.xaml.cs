3a39fa204c51b24135fcaccc059f4192274621ef
aardvark <e.a.latuhin@gmail.com>
2020-09-27 19:59:13 +0300

    Add sizing info and moving prototype

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows;
@@ -46,6 +47,21 @@ namespace window_mover
         [DllImport("user32.dll", SetLastError = true)]
         internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool GetClientRect(IntPtr hWnd, out Rect lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int Left; // x position of upper-left corner
+            public int Top; // y position of upper-left corner
+            public int Right; // x position of lower-right corner
+            public int Bottom; // y position of lower-right corner
+        }
+
         private void MinBtn_OnClick(object sender, RoutedEventArgs e)
         {
             var selected = appCbx.SelectedItem.ToString();
@@ -60,6 +76,71 @@ namespace window_mover
             var pId = int.Parse(selected.Substring(0, selected.IndexOf(":", StringComparison.Ordinal)));
             IntPtr hWnd = Process.GetProcessById(pId).MainWindowHandle;
             ShowWindow(hWnd, 3);
+        }
+
+        private void UpBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LeftBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RightBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DownBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var selected = appCbx.SelectedItem.ToString();
+            var pId = int.Parse(selected.Substring(0, selected.IndexOf(":", StringComparison.Ordinal)));
+            IntPtr hWnd = Process.GetProcessById(pId).MainWindowHandle;
+            Rect rect = getWSize(hWnd);
+            Rect csize = getCSize(hWnd);
+            MoveWindow(hWnd, rect.Left, rect.Top, csize.Right, csize.Bottom, true);
+        }
+
+        private void AppCbx_OnDropDownClosed(object o, EventArgs e)
+        {
+            var selected = appCbx.SelectedItem.ToString();
+            var pId = int.Parse(selected.Substring(0, selected.IndexOf(":", StringComparison.Ordinal)));
+            IntPtr hWnd = Process.GetProcessById(pId).MainWindowHandle;
+            Rect wsize = getWSize(hWnd);
+            /*
+             * Topleft (2202, 139) , BotRight(2982,618)
+             */
+            var _rect = $"TopLeft:({wsize.Left}, {wsize.Top}) , BotRight:({wsize.Right},{wsize.Bottom})";
+            
+            Rect csize = getCSize(hWnd);
+            /*
+             * Client: 0, 0, 765, 422
+             */
+            var cRect = $"Height: {csize.Bottom}, Width: {csize.Right}";
+            infoTbx.Text = $"{_rect}\n{cRect}";
+        }
+
+
+        private Rect getWSize(IntPtr hWnd)
+        {
+            if (!GetWindowRect(hWnd, out Rect rct))
+            {
+                MessageBox.Show("ERROR");
+            }
+
+            return rct;
+        }
+
+        private Rect getCSize(IntPtr hWnd)
+        {
+            if (!GetClientRect(hWnd, out Rect rct))
+            {
+                MessageBox.Show("ERROR");
+            }
+
+            return rct;
         }
     }
 }
