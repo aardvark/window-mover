4f5c30b3a8195f54bde19062691310d81bc0523b
aardvark <e.a.latuhin@gmail.com>
2020-09-28 21:17:47 +0300

    We can now move window almost without hacks.
    +16, +59 strange magical numbers we need to pass to the
    new window sizes to not break
    existing window size.
    I have no idea what I am doing.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace window_mover
 {
@@ -16,33 +10,8 @@ namespace window_mover
     /// </summary>
     public partial class MainWindow
     {
-        public ComboBox AppCbx
-        {
-            get => appCbx;
-            set => appCbx = value;
-        }
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            ProcessesWithWindows();
-        }
-
-        private void ProcessesWithWindows()
-        {
-            foreach (var process in Process.GetProcesses())
-            {
-                if (process.MainWindowHandle != IntPtr.Zero)
-                {
-                    appCbx.Items.Add($"{process.Id}:{process.ProcessName}, {process.MainWindowTitle}");
-                }
-            }
-        }
-
-
         [DllImport("user32.dll", SetLastError = true)]
         internal static extern bool MoveWindow(IntPtr hWnd, int x, int y, int nWidth, int nHeight, bool bRepaint);
-
 
         [DllImport("user32.dll", SetLastError = true)]
         internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
@@ -62,64 +31,159 @@ namespace window_mover
             public int Bottom; // y position of lower-right corner
         }
 
-        private void MinBtn_OnClick(object sender, RoutedEventArgs e)
+        public MainWindow()
         {
-            var selected = appCbx.SelectedItem.ToString();
+            InitializeComponent();
+            ProcessesWithWindows();
+        }
+
+        private void ProcessesWithWindows()
+        {
+            foreach (var process in Process.GetProcesses())
+            {
+                if (process.MainWindowHandle != IntPtr.Zero)
+                {
+                    appCbx.Items.Add($"{process.Id}:{process.ProcessName}, {process.MainWindowTitle}");
+                }
+            }
+        }
+
+        private static IntPtr GetMainWindowHandle(object item)
+        {
+            if (item == null)
+            {
+                return IntPtr.Zero;
+            }
+
+            var selected = item.ToString();
             var pId = int.Parse(selected.Substring(0, selected.IndexOf(":", StringComparison.Ordinal)));
             IntPtr hWnd = Process.GetProcessById(pId).MainWindowHandle;
+            return hWnd;
+        }
+
+        private void MinBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var item = appCbx.SelectedItem;
+            IntPtr hWnd = GetMainWindowHandle(item);
+            if (hWnd == IntPtr.Zero)
+            {
+                return;
+            }
+
             ShowWindow(hWnd, 6);
         }
 
         private void MaxBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            var selected = appCbx.SelectedItem.ToString();
-            var pId = int.Parse(selected.Substring(0, selected.IndexOf(":", StringComparison.Ordinal)));
-            IntPtr hWnd = Process.GetProcessById(pId).MainWindowHandle;
+            var item = appCbx.SelectedItem;
+            IntPtr hWnd = GetMainWindowHandle(item);
+            if (hWnd == IntPtr.Zero)
+            {
+                return;
+            }
+
             ShowWindow(hWnd, 3);
+        }
+
+
+        private void MoveWindowX(int dx)
+        {
+            var item = appCbx.SelectedItem;
+
+            IntPtr hWnd = GetMainWindowHandle(item);
+            if (hWnd == IntPtr.Zero)
+            {
+                return;
+            }
+
+            Rect rect = getWSize(hWnd);
+            Rect csize = getCSize(hWnd);
+
+            var x = rect.Left + dx;
+            var y = rect.Top;
+            var nWidth = csize.Right;
+            var nHeight = csize.Bottom;
+
+            MoveWindowCall(hWnd, x, y, nWidth, nHeight);
+        }
+
+        private void MoveWindowY(int dy)
+        {
+            var item = appCbx.SelectedItem;
+
+            IntPtr hWnd = GetMainWindowHandle(item);
+            if (hWnd == IntPtr.Zero)
+            {
+                return;
+            }
+
+            Rect rect = getWSize(hWnd);
+            Rect csize = getCSize(hWnd);
+
+            var x = rect.Left;
+            var y = rect.Top + dy;
+            var nWidth = csize.Right;
+            var nHeight = csize.Bottom;
+
+            MoveWindowCall(hWnd, x, y, nWidth, nHeight);
         }
 
         private void UpBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
-        }
-
-        private void LeftBtn_OnClick(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void RightBtn_OnClick(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
+            MoveWindowY(-10);
         }
 
         private void DownBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            var selected = appCbx.SelectedItem.ToString();
-            var pId = int.Parse(selected.Substring(0, selected.IndexOf(":", StringComparison.Ordinal)));
-            IntPtr hWnd = Process.GetProcessById(pId).MainWindowHandle;
-            Rect rect = getWSize(hWnd);
-            Rect csize = getCSize(hWnd);
-            MoveWindow(hWnd, rect.Left, rect.Top, csize.Right, csize.Bottom, true);
+            MoveWindowY(10);
+        }
+
+
+        private void LeftBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            MoveWindowX(-10);
+        }
+
+        private void RightBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            MoveWindowX(10);
+        }
+
+        /*
+         * Need to add next two to the new window size or it will shrink for some reason
+         * probably something to do with GetClientRect return. It totally return not what
+         * i am expecting
+         */
+        private readonly int move_window_magic_x = 16;
+        private readonly int move_window_magic_y = 59;
+
+        private bool MoveWindowCall(IntPtr hWnd, int x, int y, int nWidth, int nHeight)
+        {
+            return MoveWindow(hWnd, x, y, nWidth + move_window_magic_x, nHeight + move_window_magic_y, true);
         }
 
         private void AppCbx_OnDropDownClosed(object o, EventArgs e)
         {
-            var selected = appCbx.SelectedItem.ToString();
-            var pId = int.Parse(selected.Substring(0, selected.IndexOf(":", StringComparison.Ordinal)));
-            IntPtr hWnd = Process.GetProcessById(pId).MainWindowHandle;
+            var item = appCbx.SelectedItem;
+
+            IntPtr hWnd = GetMainWindowHandle(item);
+            if (hWnd == IntPtr.Zero)
+            {
+                return;
+            }
+
             Rect wsize = getWSize(hWnd);
             /*
              * Topleft (2202, 139) , BotRight(2982,618)
              */
-            var _rect = $"TopLeft:({wsize.Left}, {wsize.Top}) , BotRight:({wsize.Right},{wsize.Bottom})";
-            
+            var rect = $"TopLeft:({wsize.Left}, {wsize.Top}) , BotRight:({wsize.Right},{wsize.Bottom})";
+
             Rect csize = getCSize(hWnd);
             /*
              * Client: 0, 0, 765, 422
              */
             var cRect = $"Height: {csize.Bottom}, Width: {csize.Right}";
-            infoTbx.Text = $"{_rect}\n{cRect}";
+            infoTbx.Text = $"{rect}\n{cRect}";
         }
 
 
